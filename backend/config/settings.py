103c8542f5bba4d94805fe78b6687e2a55f5514c
vel-dora <katendepeterson7@gmail.com>
2026-07-24 11:38:13 +0300

    Fix typo in INSTALLED_APPS for Django REST framework

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party
-    'rest_framework',
+    'djangorestframework',
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
